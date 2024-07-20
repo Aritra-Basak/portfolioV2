60bf59a9a7cfaf0a50b86131d5efee31a99956a8
Aritra-Basak <basakaritra10@gmail.com>
2024-07-20 14:40:55 +0530

    Changed the email ID for receiving and sending reply to the users

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -30,7 +30,7 @@ export const sendEmail = async (formData: FormData) => {
     try {
         data = await resend.emails.send({
             from: "Contact Form <onboarding@resend.dev>",
-            to: "sohampal1108@gmail.com",
+            to: "basakaritra10@gmail.com",
             subject: "Message from Protfolio Contact Form",
             reply_to: senderEmail,
             react: React.createElement(ContactFormEmail, {
